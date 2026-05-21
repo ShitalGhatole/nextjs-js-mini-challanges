7b9c56905fc8048fc1688477beccf6215853dcd6
Shital K. Ghatole <shitalghatole8@gmail.com>
2026-05-21 19:43:36 +0530

    feat: add weather app fixes

--- public/vanilla-js/medium/weather-app/script.js
+++ public/vanilla-js/medium/weather-app/script.js
@@ -1,72 +1,104 @@
 let cityInput = document.getElementById("cityInput");
-let searchBtn = document.getElementById("searchBtn");
 let cityNameOptionsWrapper = document.getElementById("cityNameOptions");
 let headingCityName = document.querySelector(".headingCityName");
 let headingDegree = document.querySelector(".headingDegree");
 let description = document.querySelector(".description");
+let weatherIcon = document.querySelector(".weatherImage");
 
-const apiKey = "apikey";
 // example API
 // https://api.openweathermap.org/data/2.5/weather?lat=37.1283343&lon=-84.0835576&appid=apikey
+let availableCities = [];
 
 const capitalizeWord = (string) => {
   return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
 }
 
-async function getCityName(city) {
-  let cityName = await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${apiKey}`);
-  let json = await cityName?.json();
+async function getCityNames(city) {
+  try {
+    let cityName = await fetch(`/api/cities?cityName=${city}`);
+    let json = await cityName?.json();
+  
+    // always return an array
+    if (!Array.isArray(json)) {
+      return [];
+    }
+  
+    availableCities = json?.map((city) => {
 
-  // always return an array
-  if (!Array.isArray(json)) {
+      const displayName = city.state
+        ? `${city.name}, ${city.state}, ${city.country}`
+        : `${city.name}, ${city.country}`;
+
+      return {
+        lat: city.lat,
+        lon: city.lon,
+        name: city.name,
+        state: city.state,
+        country: city.country,
+        displayName
+      }
+    })
+    console.log("Available CIties: ", availableCities)
+    return availableCities;
+  } catch (error) {
+    console.log(error)
     return [];
   }
 
-  const availableCities = json?.map((city) => {
-    return {
-      name: city.name,
-      state: city.state,
-      country: city.country,
-      lat: city.lat,
-      lon: city.lon
-    }
-  })
-
-  return availableCities;
 }
 
-async function getCityWeather(city, state, country) {
-  const weather = await fetch(`
-    https://api.openweathermap.org/data/2.5/weather?q=${city},${state},${country}&units=metric&appid=${apiKey}
-  `);
-  const weatherData = await weather.json();
-  return weatherData;
+async function getCityWeather(lon, lat) {
+  try {
+    const res = await fetch(`/api/weather?lon=${lon}&lat=${lat}`)
+    const json = await res.json()
+    return json
+  } catch (error) {
+    console.log(error)
+  }
 }
 
 cityInput.addEventListener("input", async function () {
   const city = cityInput.value;
-  if (city === '') return
+  
+  if (!city.trim()) return
 
-  const cityName = await getCityName(city);
+  const cityNames = await getCityNames(city);
+  console.log("CITY NAMES: ",cityNames)
   cityNameOptionsWrapper.innerHTML = "";
-  cityName.forEach(city => {
+  cityNames.forEach(city => {
     cityNameOptionsWrapper.innerHTML += `
-      <option value="${city.name}, ${city.state}, ${city.country}" />
+      <option value="${city.displayName}" />
     `
   })
 })
 
 cityInput.addEventListener("change", async function () {
-  const city = cityInput.value;
-  const cityArr = city.split(",");
-  const [cityName, stateName, countryName] = cityArr;
-  const cityWeather = await getCityWeather(cityName, stateName, countryName);
+  const selectedValue = cityInput.value;
+
+  const selectedCity = availableCities.find(city => {
+    return selectedValue === city.displayName
+  })
+
+  if (!selectedCity) {
+    console.log("City not found")
+    return
+  }
+
+  const cityWeather = await getCityWeather(selectedCity.lon, selectedCity.lat);
+
+  if (!cityWeather || !cityWeather.main || !cityWeather.weather) {
+    console.log("City weather not found")
+    return
+  }
+
   const finalWeather = {
     temp: cityWeather.main.temp,
+    currentTemp: cityWeather.main.temp,
     description: capitalizeWord(cityWeather.weather[0].description),
-  }
-  console.log(cityWeather)
-  headingCityName.textContent = `${cityName}, ${countryName}`
-  headingDegree.textContent = `${finalWeather.temp} deg`
+  };
+
+  headingCityName.textContent = `${selectedCity.name}, ${selectedCity.country}`
+  headingDegree.textContent = `${Math.round(finalWeather.temp)}°C`
   description.textContent = `${finalWeather.description}`
+  weatherIcon.src = `http://openweathermap.org/img/wn/${cityWeather.weather[0].icon}@2x.png`
 })
