dffbd1013ef949bae0f90ee3a925ba2a3dc807db
Shital K. Ghatole <shitalghatole8@gmail.com>
2026-05-22 16:46:06 +0530

    feat: complete weather app (js version)

--- public/vanilla-js/medium/weather-app/script.js
+++ public/vanilla-js/medium/weather-app/script.js
@@ -1,9 +1,13 @@
 let cityInput = document.getElementById("cityInput");
-let cityNameOptionsWrapper = document.getElementById("cityNameOptions");
+let cityNameOptionsWrapper = document.getElementById("cityOptions");
 let headingCityName = document.querySelector(".headingCityName");
 let headingDegree = document.querySelector(".headingDegree");
 let description = document.querySelector(".description");
 let weatherIcon = document.querySelector(".weatherImage");
+let feelsLike = document.querySelector(".feelsLike p");
+let minTemp = document.querySelector(".minTemp p");
+let maxTemp = document.querySelector(".maxTemp p");
+let humidity = document.querySelector(".humidity p");
 
 // example API
 // https://api.openweathermap.org/data/2.5/weather?lat=37.1283343&lon=-84.0835576&appid=apikey
@@ -60,20 +64,28 @@ async function getCityWeather(lon, lat) {
 cityInput.addEventListener("input", async function () {
   const city = cityInput.value;
   
-  if (!city.trim()) return
+  if (!city.trim()) {
+    cityOptionsWrapper.innerHTML = "";    
+    return
+  }
 
   const cityNames = await getCityNames(city);
   console.log("CITY NAMES: ",cityNames)
-  cityNameOptionsWrapper.innerHTML = "";
-  cityNames.forEach(city => {
-    cityNameOptionsWrapper.innerHTML += `
-      <option value="${city.displayName}" />
-    `
-  })
+  cityNameOptionsWrapper.innerHTML = cityNames.map(city => `
+    <div class="cityOption">
+      ${city.displayName}
+    </div>  
+  `).join("");
 })
 
-cityInput.addEventListener("change", async function () {
-  const selectedValue = cityInput.value;
+cityNameOptionsWrapper.addEventListener("click", async function (e) {
+  const option = e.target.closest(".cityOption");
+
+  if (!option) return;
+
+  const selectedValue = option.textContent.trim();
+
+  cityInput.value = selectedValue;
 
   const selectedCity = availableCities.find(city => {
     return selectedValue === city.displayName
@@ -95,10 +107,25 @@ cityInput.addEventListener("change", async function () {
     temp: cityWeather.main.temp,
     currentTemp: cityWeather.main.temp,
     description: capitalizeWord(cityWeather.weather[0].description),
+    feelsLike: cityWeather.main.feels_like,
+    minTemp: cityWeather.main.temp_min,
+    maxTemp: cityWeather.main.temp_max,
+    humidity: cityWeather.main.humidity,
   };
 
-  headingCityName.textContent = `${selectedCity.name}, ${selectedCity.country}`
+  const cityNameDisplay = `${
+    selectedCity.state 
+      ? `${selectedCity.name}, ${selectedCity.state}, ${selectedCity.country}` 
+      : `${selectedCity.name}, ${selectedCity.country}`
+    }`
+
+  headingCityName.textContent = `${cityNameDisplay}`
   headingDegree.textContent = `${Math.round(finalWeather.temp)}°C`
   description.textContent = `${finalWeather.description}`
-  weatherIcon.src = `http://openweathermap.org/img/wn/${cityWeather.weather[0].icon}@2x.png`
+  weatherIcon.src = `https://openweathermap.org/img/wn/${cityWeather.weather[0].icon}@2x.png`
+  feelsLike.textContent = `${Math.round(finalWeather.feelsLike)}°C`
+  minTemp.textContent = `${Math.round(finalWeather.minTemp)}°C`
+  maxTemp.textContent = `${Math.round(finalWeather.maxTemp)}°C`
+  humidity.textContent = `${finalWeather.humidity}%`
+  cityNameOptionsWrapper.innerHTML = "";
 })
